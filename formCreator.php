79735732718f053321e81a672931a8939f86bb78
The0mikkel <mikkel.alb@gmail.com>
2020-07-04 00:07:49 +0200

    Added possebility to add new elements to forms

--- formCreator.php
+++ formCreator.php
@@ -60,7 +60,7 @@
             echo "<div id='edit-form-$tableId' class='formCreator_edit_container'>";
             // Column info
             $table_name = $wpdb->prefix . 'htx_column';
-            $stmt = $link->prepare("SELECT * FROM `$table_name` WHERE tableid = ? AND adminOnly = 0");
+            $stmt = $link->prepare("SELECT * FROM `$table_name` WHERE tableid = ? AND adminOnly = 0 ORDER by sorting");
             $stmt->bind_param("i", $tableId);
             $stmt->execute();
             $result = $stmt->get_result();
@@ -96,6 +96,67 @@
                 }
             }
             $stmt->close();
+
+            // Create new row
+            echo "<form method=\"post\" class='addColumn'>";
+            echo "<h4>Tilføj ny række</h4>";
+            // Drop down with possible types of input field
+            echo "<label>Input type: </label><br><select name='inputType'><option value='inputbox'>Input box</option><option value='dropdown'>Drop down</option></select><br>";
+            echo "<button type='submit' name='submit' value='newColumn' style='margin-top: 0.25rem'>Tilføj række</button>";
+            echo "</form>";
+
+            // Post handling from form
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                switch  ($_POST['submit']) {
+                    // New submission
+                    case 'newColumn':
+                        try {
+                            $link->autocommit(FALSE); //turn on transactions
+                            // Possible input types in array
+                            $possibleInput = array("inputBox", "dropdown");
+                            // User input
+                            $userInputType = $_POST['inputType'];
+                            // Break if the user input is not known
+                            if (!in_array($userInputType, $possibleInput)) break;
+                            // Define values for new element
+                            $columnNameFront = "New element"; $format="text"; $columnType=$userInputType; $special=0; $specialName=""; 
+                            $placeholderText = ""; $adminOnly = 0; $required = 0; $settingCat = 0; 
+                            if ($userInputType == 'dropdown') {
+                                // If dropdown, then make setting category first
+                                $table_name = $wpdb->prefix . 'htx_settings_cat';
+                                $stmt = $link->prepare("INSERT INTO $table_name (tableId, settingNameBack, settingType) VALUES (?, ?, ?)");
+                                $stmt->bind_param("iss", $tableId, $columnNameBack, $columnType);
+                                $stmt->execute();
+                                $settingCat = intval($link->insert_id);
+                                if ($settingCat < 0) throw new Exception('Setting cat is bad');
+                                $stmt->close();
+                            }
+                            
+                            // Create new column, with standard values, and user input
+                            global $wpdb;
+                            $table_name = $wpdb->prefix . 'htx_column';
+                            $stmt = $link->prepare("INSERT INTO $table_name (tableId, columnNameFront, format, columnType, special, specialName, sorting, placeholderText, adminOnly, required, settingCat) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+                            $stmt->bind_param("isssssissii", $tableId, $columnNameFront, $format, $columnType, $special, $specialName, $sorting, $placeholderText, $adminOnly, $required, $settingCat);
+                            $stmt->execute();
+                            $lastId = intval($link->insert_id);
+                            if ($lastId < 0) throw new Exception('Id is bad');
+                            $stmt->close();
+                            $stmt = $link->prepare("UPDATE $table_name SET columnNameBack = ? WHERE id = ?");
+                            $stmt->bind_param("ii", $lastId, $lastId);
+                            $stmt->execute();
+                            $stmt->close();
+
+                            $link->autocommit(TRUE); //turn off transactions + commit queued queries
+
+                            // Alert user through prompt
+                            echo "<script>confirm('Nyt element tilføjet'); location.reload();</script>"; #Because this post is after the form, there may be problems with not loading in
+                        } catch(Exception $e) {
+                            $mysqli->rollback(); //remove all queries from queue if error (undo)
+                            throw $e;
+                        }
+                    break;
+                }
+            }
 
             // End div
             echo "</div>";
@@ -136,22 +197,9 @@
                     // Write
                     echo "<div id='settingEdit-$settingTableId-$settingId'><h3>$columnNameFront</h3>";
                     switch ($columnType) {
-                        case "inputbox":
-                            echo "<div class='formCreator_edit_container formCreator_flexRow'>";
-                            echo "<div><p>Navn</p><input class='inputBox' value='$columnNameFront'></div>";
-                            echo "<div><p>format</p><input class='inputBox' value='$format'></div>";
-                            echo "<div><p>columnType</p><input class='inputBox' value='$columnType'></div>";
-                            echo "<div><p>special</p><input class='inputBox' value='$special'></div>";
-                            echo "<div><p>specialName</p><input class='inputBox' value='$specialName'></div>";
-                            echo "<div><p>placeholderText</p><input class='inputBox' value='$placeholderText'></div>";
-                            echo "<div><p>sorting</p><input class='inputBox' value='$sorting'></div>";
-                            echo "<div><p>required</p><input class='inputBox' value='$required'></div>";
-                            echo "</div>";
-                        break;
                         case "dropdown":
                             echo "<div class='formCreator_edit_container formCreator_flexRow'>";
                             echo "<div><p>Navn</p><input class='inputBox' value='$columnNameFront'></div>";
-                            echo "<div><p>Navn (backend)</p><input class='inputBox' value='$columnNameBack'></div>";
                             echo "<div><p>format</p><input class='inputBox' value='$format'></div>";
                             echo "<div><p>columnType</p><input class='inputBox' value='$columnType'></div>";
                             echo "<div><p>special</p><input class='inputBox' value='$special'></div>";
@@ -192,6 +240,18 @@
                                 }
                             }
                             $stmt2->close();
+                            echo "</div>";
+                        break;
+                        default:
+                            echo "<div class='formCreator_edit_container formCreator_flexRow'>";
+                            echo "<div><p>Navn</p><input class='inputBox' value='$columnNameFront'></div>";
+                            echo "<div><p>format</p><input class='inputBox' value='$format'></div>";
+                            echo "<div><p>columnType</p><input class='inputBox' value='$columnType'></div>";
+                            echo "<div><p>special</p><input class='inputBox' value='$special'></div>";
+                            echo "<div><p>specialName</p><input class='inputBox' value='$specialName'></div>";
+                            echo "<div><p>placeholderText</p><input class='inputBox' value='$placeholderText'></div>";
+                            echo "<div><p>sorting</p><input class='inputBox' value='$sorting'></div>";
+                            echo "<div><p>required</p><input class='inputBox' value='$required'></div>";
                             echo "</div>";
                         break;
                     }
