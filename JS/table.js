8742a9a503ca3274a7d6e130a0b7785c7a5cb258
The0mikkel <mikkel.alb@gmail.com>
2020-07-20 21:34:19 +0200

    Added table sort function, for when you have thead and tbody

--- JS/table.js
+++ JS/table.js
@@ -37,3 +37,49 @@ function confirmDelete(id) {
         document.forms[id].submit();
     }
 }
+
+// Sort table (edited https://stackoverflow.com/questions/24033294/sorting-table-rows-according-to-table-header-column-using-javascript-or-jquery)
+//  sortTable(f,n)
+//  f : 1 ascending order, -1 descending order
+//  n : n-th child(<td>) of <tr>
+function sortTable(f,n, tableId){
+    var rows = $('#'+tableId+' tbody  tr').get();
+
+    rows.sort(function(a, b) {
+
+        var A = getVal(a);
+        var B = getVal(b);
+
+        if(A < B) {
+            return -1*f;
+        }
+        if(A > B) {
+            return 1*f;
+        }
+        return 0;
+    });
+
+    function getVal(elm){
+        var v = $(elm).children('td').eq(n).text().toUpperCase();
+        if($.isNumeric(v)){
+            v = parseInt(v,10);
+        }
+        return v;
+    }
+
+    $.each(rows, function(index, row) {
+        $('#'+tableId).children('tbody').append(row);
+    });
+}
+var f_sl = 1; // flag to toggle the sorting order
+var f_nm = 1; // flag to toggle the sorting order
+$("#sl").click(function(){
+    f_sl *= -1; // toggle the sorting order
+    var n = $(this).prevAll().length;
+    sortTable(f_sl,n);
+});
+$("#nm").click(function(){
+    f_nm *= -1; // toggle the sorting order
+    var n = $(this).prevAll().length;
+    sortTable(f_nm,n);
+});
