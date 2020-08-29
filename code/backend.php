77e5edef2cd085d3fbcffc697a1db40b4f27e3e8
The0mikkel <mikkel.alb@gmail.com>
2020-08-29 22:54:14 +0200

    fix error with database upgrade

--- code/backend.php
+++ code/backend.php
@@ -145,13 +145,13 @@
         // Database upgrade
         // if ($serverDatabaseversion != $databaseVersion)
         echo "<p>database version: 0</p>";
-        echo "<form method='post'><button type='submit' class='btn updateBtn' name='database_upgrade' style='margin-bottom: 0.5rem;' onclick='HTXJS_upgradeDatabases()'>Opgrader database</button><br></form>";
-        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['postType'])) {
+        echo "<form method='post'><button type='submit' class='btn updateBtn' name='submit' value='database_upgrade' style='margin-bottom: 0.5rem;' onclick='HTXJS_upgradeDatabases()'>Opgrader database</button><br></form>";
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
             if(current_user_can("manage_options")){
-                switch($_POST['postType']) {
+                switch($_POST['submit']) {
                     case 'database_upgrade':
                         echo "<br>database upgrade in progress";
-                        $update = true;
+                        $update = false;
 
                         try {
                             global $wpdb;
@@ -159,17 +159,20 @@
                             $link->autocommit(FALSE); //turn on transactions
 
                             $table_name = $wpdb->prefix . 'htx_settings';
-                            $stmt = $link->prepare("SELECT * FROM $table_name WHERE settingName = 'databaseVersion' and active = 2, and type = 'databaseVersion'");
+                            $stmt = $link->prepare("SELECT * FROM $table_name WHERE settingName = 'databaseVersion' and active = 2 and type = 'databaseVersion'");
+                            if(!$stmt)
+                                throw new Exception($link->error);
                             $stmt->execute();
                             $result = $stmt->get_result();
-                            if($result->num_rows === 0) $update = false;
+                            if($result->num_rows === 0) $update = true;
                             $stmt->close();
 
                             $link->autocommit(TRUE); //turn off transactions + commit queued queries
                             $link->close();
                         } catch(Exception $e) {
                             $link->rollback(); //remove all queries from queue if error (undo)
-                            echo "<br> Update failed.";
+                            echo "<br> Update failed. <br>Error:<br>".$e;
+                            $update = false;
                         }
 
                         if ($update == false) {
@@ -182,7 +185,9 @@
 
                                 echo "<br> Database is not up to date, upgrading...";
                                 $table_name = $wpdb->prefix . 'htx_settings';
-                                $stmt = $link->prepare("INSERT INTO $table_name (settingId, tableId, active, settingName, value, expence, special, specialName, type, sorting) VALUES (0, 0, 2, 'databaseVersion', 0.1, 0, 'databaseVersion', 'databaseVersion', 'databaseVersion', 0)");
+                                $stmt = $link->prepare("INSERT INTO $table_name (settingId, tableId, active, settingName, value, expence, specialName, type, sorting) VALUES (0, 0, 2, 'databaseVersion', '0.1', 0, 'databaseVersion', 'databaseVersion', 0)");
+                                if(!$stmt)
+                                    throw new Exception($link->error);
                                 $stmt->execute();
                                 $stmt->close();
 
@@ -191,6 +196,7 @@
                             } catch(Exception $e) {
                                 $link->rollback(); //remove all queries from queue if error (undo)
                                 echo "<br> Update failed.";
+                                echo "<br>Error:<br>".$e;
                             }
                             echo "<br> Database is now updated";
                         }
