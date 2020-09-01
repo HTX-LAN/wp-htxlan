b0101a6724c49cba2ab1c5457cda3f6ae881453d
The0mikkel <mikkel.alb@gmail.com>
2020-09-01 16:36:08 +0200

    Updated database version upgrade system

--- code/db.php
+++ code/db.php
@@ -7,7 +7,7 @@ if(!defined('ABSPATH')) {
 }
     // Upgrading of database
     function upgrade_db() {
-        $databaseVersion = "0.1";
+        $databaseVersion = 0.1;
         echo "<h4>Database status</h4>";
 
         // Check version number
@@ -23,20 +23,22 @@ if(!defined('ABSPATH')) {
                 throw new Exception($link->error);
             $stmt->execute();
             $result = $stmt->get_result();
-            if($result->num_rows === 0) $db_error = true;
-            while($row = $result->fetch_assoc()) {
-                $serverDatabaseversion = $row['value'];
+            if($result->num_rows === 0) $db_error = true; else {
+                while($row = $result->fetch_assoc()) {
+                    $serverDatabaseversion = floatval($row['value']);
+                }
             }
             $stmt->close();
 
             $link->autocommit(TRUE); //turn off transactions + commit queued queries
             $link->close();
         } catch(Exception $e) {
+            $db_error = true;
             $link->rollback(); //remove all queries from queue if error (undo)
             echo "<p style='color: red'><br>There was an error with the database, please reset database!<br>Error message from server:".$e."</p>";
         }
 
-        if ($serverDatabaseversion != $databaseVersion) {
+        if ($serverDatabaseversion != $databaseVersion and $db_error == false) {
             // Update database
             try {
                 echo "<p style='color: orange'>Database update in progress...</p>";
@@ -44,6 +46,20 @@ if(!defined('ABSPATH')) {
                 $link = database_connection();
                 $link->autocommit(FALSE); //turn on transactions
 
+                // Insert upgrade forms here
+                if ($serverDatabaseversion < 0.1) {
+                    // Some action here to update databse to 0.1
+
+                    // Update version number
+                    $table_name = $wpdb->prefix . 'htx_settings';
+                    $stmt = $link->prepare("UPDATE $table_name SET value = '0.1' WHERE settingName = 'databaseVersion' and active = 2 and type = 'databaseVersion'");
+                    if(!$stmt)
+                        throw new Exception($link->error);
+                    $stmt->execute();
+                    $stmt->close();
+
+                    $serverDatabaseversion = 0.1;
+                }
 
                 // Update version number
                 $table_name = $wpdb->prefix . 'htx_settings';
@@ -52,8 +68,6 @@ if(!defined('ABSPATH')) {
                     throw new Exception($link->error);
                 $stmt->bind_param("s", $databaseVersion);
                 $stmt->execute();
-                $result = $stmt->get_result();
-                if($result->num_rows === 0) $update = true;
                 $stmt->close();
 
                 $link->autocommit(TRUE); //turn off transactions + commit queued queries
@@ -62,8 +76,10 @@ if(!defined('ABSPATH')) {
             } catch(Exception $e) {
                 $link->rollback(); //remove all queries from queue if error (undo)
                 echo "<br> Update failed. <br>Error:<br>".$e;
-                $update = false;
             }
+        } else if ($db_error == true) {
+            echo "<p style='color: red'><br>There was an error with the database, please reset database!</p>";
+            
         } else {
             echo "<p style='color: green'>Database is up to date</p>";
         }
